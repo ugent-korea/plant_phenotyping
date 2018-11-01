5b655d859bd7567d171aaffd9b3c6004fed7d6ea
vandeb69 <br.vandenberghe@gmail.com>
2018-11-01 13:59:58 +0900

    added functions for undistortion and marker detection

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from PIL import Image
 import shutil
 import json
 import os
+import pickle
 
 DIR = "./photos/plant1_20180615/entire_plant"
 APP_DIR = "C:/Users/Breght/Documents/Doctoraat/Annotator"
@@ -81,6 +82,72 @@ def batch_threshold_segmentation(limits=([0, 255], [54, 255], [0, 255]), colour_
         threshold_segmentation(in_f, out_f, limits, colour_space)
 
 
+def undistortion_parameters(cal_f):
+    dims = (7, 9)
+    coords_3d = np.zeros((dims[0]*dims[1], 3), np.float32)
+    coords_3d[:, :2] = np.mgrid[0:dims[0], 0:dims[1]].T.reshape(-1, 2)
+
+    squares_3d = []
+    squares_2d = []
+    img = cv2.imread(cal_f, 0)
+    ret, img = cv2.threshold(img, 80, 255, cv2.THRESH_BINARY)
+    ret, corners = cv2.findChessboardCorners(img, dims, None)
+    if ret:
+        squares_3d.append(coords_3d)
+        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+        corners = cv2.cornerSubPix(img, corners, (11, 11), (-1, -1), criteria)
+        squares_2d.append(corners)
+        ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(squares_3d, squares_2d, img.shape[::-1], None, None)
+        h, w = img.shape[:2]
+        newmtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
+        mapx, mapy = cv2.initUndistortRectifyMap(mtx, dist, None, newmtx, (w, h), 5)
+        return mapx, mapy, roi
+    else:
+        raise Exception("Chessboard detection failed")
+
+
+def undistortion(in_f, out_f, mapx, mapy, roi, crop=True):
+    img = cv2.imread(in_f, 1)
+    undist = cv2.remap(img, mapx, mapy, cv2.INTER_LINEAR)
+    if crop:
+        x, y, w, h = roi
+        undist = undist[y:y+h, x:x+w]
+    cv2.imwrite(out_f, undist)
+    file_message(out_f)
+
+
+def batch_undistortion(cal_dir=CAL_DIR, in_dir=IMG_DIR, out_dir=UND_DIR, crop=True):
+    ensure_dir(out_dir)
+
+    cal_f = files(dir=cal_dir, ext=".jpg")[0]
+    mapx, mapy, roi = undistortion_parameters(cal_f)
+    for in_f, out_f in files(in_dir=in_dir, out_dir=out_dir, in_ext=".jpg", out_ext=".jpg"):
+        undistortion(in_f, out_f, mapx, mapy, roi, crop)
+
+
+def detect_markers(in_f, out_f, aruco_dict, aruco_params, show=False):
+    img = cv2.imread(in_f, cv2.IMREAD_COLOR)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    corners, ids, rejected = cv2.aruco.detectMarkers(gray, aruco_dict, parameters=aruco_params)
+    with open(out_f, "wb") as out:
+        pickle.dump([corners, ids, rejected], out)
+    file_message(out_f)
+
+    if show:
+        img_with_aruco = cv2.aruco.drawDetectedMarkers(img, corners, ids, (0, 255, 0))
+        cv2.imshow("aruco", img_with_aruco)
+        cv2.waitKey(0)
+
+
+def batch_detect_markers(in_dir=UNDCR_DIR, out_dir=ARUCO_DIR, show=True):
+    ensure_dir(out_dir)
+
+    aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_7X7_1000)
+    aruco_params = cv2.aruco.DetectorParameters_create()
+    for in_f, out_f in files(in_dir=in_dir, out_dir=out_dir, in_ext=".jpg", out_ext=".pkl"):
+        detect_markers(in_f, out_f, aruco_dict=aruco_dict, aruco_params=aruco_params, show=show)
+
+
 def resize(in_f, out_f, ratio=.5):
     img = cv2.imread(in_f, 1)
     dim = (int(ratio * img.shape[1]), int(ratio * img.shape[0]))
@@ -137,3 +204,9 @@ if __name__ == "__main__":
     #   and move some images and corresponding annotations in the folder,
     #   then run the prepare_annotator function
     prepare_annotator(IMG_TRANSFER_DIR, ANN_TRANSFER_DIR, APP_DIR, ("background", "plant", "panicle"))
+
+    # step 5: do undistortion
+    batch_undistortion(CAL_DIR, IMG_DIR, UND_DIR, crop=False)
+    batch_undistortion(CAL_DIR, IMG_DIR, UNDCR_DIR, crop=True)
+    # step 6: detect markers
+    batch_detect_markers(UNDCR_DIR, ARUCO_DIR)
